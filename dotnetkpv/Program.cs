37a612e0252a4ac4bec85afd7e1a834ee3631a67
ArturKL <arturkalimullin10@gmail.com>
2026-09-03 21:39:22 +0300

    initial

---
+++ dotnetkpv/Program.cs
@@ -0,0 +1,13 @@
+﻿string Reverse(string s)
+{
+    return string.Join("", s.Reverse());
+}
+
+// без знаков препинания
+string ReverseSentence(string s)
+{
+    return string.Join(" ", s.Split().Select(Reverse));
+}
+
+Console.WriteLine(Reverse("Hello")); // olleH
+Console.WriteLine(ReverseSentence("Hello World")); // olleH dlroW
